ef5d5420d6bd9420b4a298322c951d3ba7faca36
José Luis Gutierrez <joselugz03@gmail.com>
2023-12-30 00:52:32 -0300

    create backend project folder

---
+++ backend/src/main/java/com/alura/foro/api/AluraForoApplication.java
@@ -0,0 +1,13 @@
+package com.alura.foro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForoApplication.class, args);
+	}
+
+}
