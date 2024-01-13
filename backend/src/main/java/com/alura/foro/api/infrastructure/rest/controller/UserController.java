77fd670717cf461707622a375fdc24eea1ed2738
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-13 10:42:05 -0300

    create get and post mapping on the user controller

--- backend/src/main/java/com/alura/foro/api/infrastructure/rest/controller/UserController.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.alura.foro.api.infrastructure.rest.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,12 +10,20 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.foro.api.application.service.UserService;
+import com.alura.foro.api.domain.dto.CreateUserDTO;
 import com.alura.foro.api.domain.dto.ResponseUserDTO;
+import com.alura.foro.api.domain.model.User;
 import com.alura.foro.api.infrastructure.util.Pagination;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
@@ -34,9 +43,30 @@ public class UserController {
         userService.getAllUsers()
                 .forEach((user) -> userResponse.add(new ResponseUserDTO(user.getId(), user.getUsername(), user.getImage())));
         
-                System.out.println(userResponse);
                 Page<ResponseUserDTO> page = Pagination.convert(userResponse, pageable);
         return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseUserDTO> getUser(@PathVariable Long id) {
+        User user = this.userService.getUser(id);
+        return ResponseEntity.ok(new ResponseUserDTO(user.getId(), user.getUsername(), user.getImage()));
+    }
+    
+    @PostMapping
+    public ResponseEntity<ResponseUserDTO> createUser(@RequestBody @Valid CreateUserDTO createUserDTO,
+            UriComponentsBuilder uriComponentsBuilder) {
+
+        User user = new User();
+        user.setUsername(createUserDTO.username());
+        user.setPassword(createUserDTO.username());
+
+        User newUser = this.userService.createUser(user);
+
+        ResponseUserDTO responseUserDTO = new ResponseUserDTO(newUser.getId(), newUser.getUsername(), newUser.getImage());
+
+        URI url = uriComponentsBuilder.path("/api/user/{id}").buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(url).body(responseUserDTO);
     }
 
 }
