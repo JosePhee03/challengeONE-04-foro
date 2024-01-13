1ed7617d86c2392a2ff88cbd4b5fa62c6e032019
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-13 10:42:24 -0300

    create class response message

---
+++ backend/src/main/java/com/alura/foro/api/infrastructure/exeption/ResponseMessage.java
@@ -0,0 +1,12 @@
+package com.alura.foro.api.infrastructure.exeption;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+
+    private String message;
+
+}
