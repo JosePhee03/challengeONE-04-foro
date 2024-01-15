e2dba984b40081c35f3e76ee54a44696adbff2a0
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-15 11:06:14 -0300

    create post service bean

--- backend/src/main/java/com/alura/foro/api/infrastructure/config/ServiceBean.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/config/ServiceBean.java
@@ -3,15 +3,24 @@ package com.alura.foro.api.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.alura.foro.api.application.service.DomainPostService;
 import com.alura.foro.api.application.service.DomainUserService;
+import com.alura.foro.api.application.service.PostService;
 import com.alura.foro.api.application.service.UserService;
+import com.alura.foro.api.domain.port.PostRepository;
 import com.alura.foro.api.domain.port.UserRepository;
 
 @Configuration
 public class ServiceBean {
+
     @Bean
     UserService UserBeanService (final UserRepository userRepository) {
         return new DomainUserService(userRepository);
+    }
+
+    @Bean
+    PostService postBeanService (final PostRepository postRepository) {
+        return new DomainPostService(postRepository);
     }
 
 }
