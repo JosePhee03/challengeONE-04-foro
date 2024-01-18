14653693a9c479e3625ac490a9144f06fa7e6bab
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-18 12:10:04 -0300

    update post controller

--- backend/src/main/java/com/alura/foro/api/infrastructure/rest/controller/PostController.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/rest/controller/PostController.java
@@ -1,14 +1,12 @@
 package com.alura.foro.api.infrastructure.rest.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.foro.api.application.service.PostService;
-import com.alura.foro.api.application.service.UserService;
 import com.alura.foro.api.domain.dto.CreatePostDTO;
 import com.alura.foro.api.domain.dto.ResponsePostDTO;
-import com.alura.foro.api.domain.model.Category;
 import com.alura.foro.api.domain.model.Post;
-import com.alura.foro.api.domain.model.User;
 import com.alura.foro.api.infrastructure.util.Pagination;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/post")
@@ -35,55 +28,36 @@ import jakarta.validation.Valid;
 public class PostController {
     
     private final PostService postService;
-    private final UserService userService;
 
-    public PostController(PostService postService, UserService userService) {
+    public PostController(PostService postService) {
         this.postService = postService;
-        this.userService = userService;
     }
 
     @GetMapping
-    public ResponseEntity<Page<ResponsePostDTO>> getPost(@PageableDefault(size = 30) Pageable pageable) {
-        List<ResponsePostDTO> responsePostDTOs = new ArrayList<>();
-
-
-        postService.getAllPosts()
-                .forEach((post) -> responsePostDTOs.add(new ResponsePostDTO(post)));
+    public ResponseEntity<Page<ResponsePostDTO>> getAllPosts(@PageableDefault(size = 30) Pageable pageable) {
+        List<ResponsePostDTO> responseUserDTOs = this.postService.getAllPosts();
         
-        Page<ResponsePostDTO> page = Pagination.convert(responsePostDTOs, pageable);
+        Page<ResponsePostDTO> page = Pagination.convert(responseUserDTOs, pageable);
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponsePostDTO> getPost(@PathVariable Long id) {
-        Post post = this.postService.getPost(id);
-        return ResponseEntity.ok(new ResponsePostDTO(post));
+        ResponsePostDTO postDTO = this.postService.getPost(id);
+        return ResponseEntity.ok(postDTO);
     }
     
     @PostMapping
     public ResponseEntity<ResponsePostDTO> createPost(@RequestBody CreatePostDTO createPostDTO,
             UriComponentsBuilder uriComponentsBuilder) {
 
-        User user = userService.getUser(1L);
-
         Post post = new Post();
         post.setContent(createPostDTO.content());
         post.setTitle(createPostDTO.title());
-        Category category = new Category();
 
-        category.setId(2L);
-        category.setName("CSS");
+        ResponsePostDTO responsePostDTO = this.postService.createPost(post);
 
-        post.getCategories().add(category);
-        post.setUser(user);
-
-        Post newPost = this.postService.createPost(post);
-
-        ResponsePostDTO responsePostDTO = new ResponsePostDTO(newPost);
-
-        System.out.println("post_id: " + newPost.getId());
-
-        URI url = uriComponentsBuilder.path("/api/post/{id}").buildAndExpand(newPost.getId()).toUri();
+        URI url = uriComponentsBuilder.path("/api/post/{id}").buildAndExpand(responsePostDTO.getId()).toUri();
         return ResponseEntity.created(url).body(responsePostDTO);
     }
 
