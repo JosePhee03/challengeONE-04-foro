7509f314355f60da87d1ff9e57da492106d1a49c
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-18 19:40:44 -0300

    add post id

--- backend/src/main/java/com/alura/foro/api/domain/dto/CreateCommentDTO.java
+++ backend/src/main/java/com/alura/foro/api/domain/dto/CreateCommentDTO.java
@@ -1,9 +1,13 @@
 package com.alura.foro.api.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateCommentDTO(
+
+	@NotNull(message = "Error al encontrar la publicación")
+	Long postId,
 
     @Size(min = 8, max = 140, message = "El contenido debe tener 8 a 140 caracteres")
 	@NotBlank(message = "Campo contenido no puede estar vacío")
