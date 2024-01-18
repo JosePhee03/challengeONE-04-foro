c176ac35dd35b4c5102657211e4e471816994190
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-18 12:14:43 -0300

    update repositories with responseDTO

--- backend/src/main/java/com/alura/foro/api/domain/port/CommentRepository.java
+++ backend/src/main/java/com/alura/foro/api/domain/port/CommentRepository.java
@@ -1,14 +1,15 @@
 package com.alura.foro.api.domain.port;
 
+import com.alura.foro.api.domain.dto.ResponseCommentDTO;
 import com.alura.foro.api.domain.model.Comment;
 
 public interface CommentRepository {
     
     // POST
-	Comment createComment(Comment comment);
+	ResponseCommentDTO createComment(Comment comment);
 
 	// UPDATE
-	Comment updateComment(Long id, String content);
+	ResponseCommentDTO updateComment(Long id, String content);
 
 	// DELETE
 	void deleteComment(Long id);
