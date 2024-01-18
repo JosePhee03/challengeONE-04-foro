20f09a1d1e41089d7ec8288e224c9dec5efac6dd
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-18 14:01:58 -0300

    rename prop method

--- backend/src/main/java/com/alura/foro/api/infrastructure/adapter/CategoryJpaRepositoryMySQL.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/adapter/CategoryJpaRepositoryMySQL.java
@@ -1,5 +1,7 @@
 package com.alura.foro.api.infrastructure.adapter;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.alura.foro.api.infrastructure.entity.CategoryEntity;
 
 @Repository
 public interface CategoryJpaRepositoryMySQL extends JpaRepository<CategoryEntity, Long> {
+
+    List<CategoryEntity> findByIdIn(List<Long> categoriesId);
 
 }
