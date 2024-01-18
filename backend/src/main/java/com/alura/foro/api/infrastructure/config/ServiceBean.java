3daccd3ae7054995c620d92c6c437ca9965c5714
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-18 19:44:45 -0300

    add bean of comment service

--- backend/src/main/java/com/alura/foro/api/infrastructure/config/ServiceBean.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/config/ServiceBean.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.alura.foro.api.application.service.CategoryService;
+import com.alura.foro.api.application.service.CommentService;
 import com.alura.foro.api.application.service.DomainCategoryService;
+import com.alura.foro.api.application.service.DomainCommentService;
 import com.alura.foro.api.application.service.DomainPostService;
 import com.alura.foro.api.application.service.DomainUserService;
 import com.alura.foro.api.application.service.PostService;
 import com.alura.foro.api.application.service.UserService;
 import com.alura.foro.api.domain.port.CategoryRepository;
+import com.alura.foro.api.domain.port.CommentRepository;
 import com.alura.foro.api.domain.port.PostRepository;
 import com.alura.foro.api.domain.port.UserRepository;
 
@@ -29,6 +32,11 @@ public class ServiceBean {
     @Bean
     CategoryService categoryBeanService (final CategoryRepository categoryRepository) {
         return new DomainCategoryService(categoryRepository);
+    }
+
+    @Bean
+    CommentService commentBeanService (final CommentRepository commentRepository) {
+        return new DomainCommentService(commentRepository);
     }
 
 }
