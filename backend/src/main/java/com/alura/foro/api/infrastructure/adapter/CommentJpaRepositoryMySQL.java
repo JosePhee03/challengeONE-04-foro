abbf919d5636d8ae2b6335e674c926eb28f6bb85
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-24 12:05:31 -0300

    create query jpa for comment search

--- backend/src/main/java/com/alura/foro/api/infrastructure/adapter/CommentJpaRepositoryMySQL.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/adapter/CommentJpaRepositoryMySQL.java
@@ -1,6 +1,9 @@
 package com.alura.foro.api.infrastructure.adapter;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alura.foro.api.infrastructure.entity.CommentEntity;
@@ -8,4 +11,9 @@ import com.alura.foro.api.infrastructure.entity.CommentEntity;
 @Repository
 public interface CommentJpaRepositoryMySQL extends JpaRepository<CommentEntity, Long> {
 
+    @Query("""
+            FROM CommentEntity c
+            WHERE (:postId IS NULL OR c.postEntity.id = :postId)
+        """)
+    Page<CommentEntity> searchComments(Long postId, Pageable pageable);
 }
