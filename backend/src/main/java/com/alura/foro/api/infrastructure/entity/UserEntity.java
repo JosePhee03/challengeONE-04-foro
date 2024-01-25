1827e2e85371697b0e750d5e9610f3e6fa015a1f
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-25 14:54:52 -0300

    inplement user details on user entity

--- backend/src/main/java/com/alura/foro/api/infrastructure/entity/UserEntity.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/entity/UserEntity.java
@@ -1,7 +1,12 @@
 package com.alura.foro.api.infrastructure.entity;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.alura.foro.api.domain.model.Role;
 
@@ -16,7 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,5 +39,30 @@ public class UserEntity {
 
     @OneToMany(mappedBy = "userEntity", cascade = CascadeType.ALL)
     private List<CommentEntity> commentEntities = new ArrayList<>();
+
+        @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
