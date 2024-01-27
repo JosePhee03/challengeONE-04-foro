869a7793d4b44481943b37aa53ce9ee78f9e905a
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-27 14:51:58 -0300

    update response error message

--- backend/src/main/java/com/alura/foro/api/infrastructure/exeption/ResponseErrorMessage.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/exeption/ResponseErrorMessage.java
@@ -1,12 +1,34 @@
 package com.alura.foro.api.infrastructure.exeption;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseErrorMessage {
 
+    private LocalDateTime timestamp;
+    private int status;
     private String error;
+    private String message;
+    private String path;
+
+    public ResponseErrorMessage (
+        HttpStatusCode httpStatusCode,
+        String message,
+        String path
+    ) {
+        HttpStatus error = HttpStatus.resolve(httpStatusCode.value());
+        this.timestamp = LocalDateTime.now();
+        this.status = httpStatusCode.value();
+        this.message = message;
+        this.error = error != null ? error.getReasonPhrase() : "error";
+        this.path = path;
+    }
 
 }
