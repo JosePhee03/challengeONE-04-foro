e42095a8b458ee9aae0aed7284fd55d198f3f764
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-27 11:49:55 -0300

    update username column with unique

--- backend/src/main/resources/db/migration/V1__create_table_user.sql
+++ backend/src/main/resources/db/migration/V1__create_table_user.sql
@@ -1,7 +1,7 @@
 create table user(
 
     id bigint not null auto_increment,
-    username varchar(150) not null,
+    username varchar(150) not null unique,
     password varchar(255) not null,
     role ENUM('USER', 'ADMIN') not null,
     image varchar(255),
