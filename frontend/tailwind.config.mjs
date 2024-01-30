14d7f42d518f8f161f2a33e8165e137df60ebb47
José Luis Gutierrez <joselugz03@gmail.com>
2024-01-30 18:03:31 -0300

    update tailwind config

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			'inter': ['"Inter"', 'sans-serif'],
+		  },
+		colors: {
+			"primary":{
+				light: "rgba(0, 106, 245, 1)",
+				dark: "rgba(12, 108, 233, 1)"
+			},
+			base: {
+				light: "rgba(255, 255, 255, 1)",
+				dark: "rgba(34, 51, 72, 1)"
+			},
+			icon: {
+				light: "rgba(97, 109, 124, 1)",
+				dark: "rgba(228, 239, 252, 1)"
+			}
+		},
+		extend: {
+			strokeWidth: {
+				'3': '3',
+			},
+		},
 	},
 	plugins: [],
 }
