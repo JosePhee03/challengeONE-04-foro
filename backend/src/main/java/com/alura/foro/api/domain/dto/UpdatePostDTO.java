969f57745e6898e2b940e77f95aec63184a422f7
José Luis Gutierrez <joselugz03@gmail>
2024-05-09 21:15:04 -0300

    change nulleable option from update post dto

--- backend/src/main/java/com/alura/foro/api/domain/dto/UpdatePostDTO.java
+++ backend/src/main/java/com/alura/foro/api/domain/dto/UpdatePostDTO.java
@@ -2,6 +2,7 @@ package com.alura.foro.api.domain.dto;
 
 import java.util.Set;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,16 +11,20 @@ public record UpdatePostDTO (
     
     @Size(min = 8, max = 80, message = "El titulo debe tener 8 a 80 caracteres")
 	@NotBlank(message = "Campo titulo no puede estar vacío")
+    @Nullable
 	String title,
 
 	@Size(min = 8, max = 140, message = "El contenido debe tener 8 a 140 caracteres")
 	@NotBlank(message = "Campo contenido no puede estar vacío")
+    @Nullable
 	String content,
 
     @Size(min = 1, message = "El conjunto debe contener al menos una categoria")
+    @Nullable
     Set<Long> categories,
 
     @NotNull(message = "Falta el campo del estado")
+    @Nullable
     Boolean status
 
 ) {}
