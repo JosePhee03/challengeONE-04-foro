7fa5ecd8cb5ac05b80ac93e1731dd2282ecb28d8
José Luis Gutierrez <joselugz03@gmail>
2024-05-10 15:12:03 -0300

    accept null in update post dto

--- backend/src/main/java/com/alura/foro/api/domain/dto/UpdatePostDTO.java
+++ backend/src/main/java/com/alura/foro/api/domain/dto/UpdatePostDTO.java
@@ -3,19 +3,15 @@ package com.alura.foro.api.domain.dto;
 import java.util.Set;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdatePostDTO (
     
     @Size(min = 8, max = 80, message = "El titulo debe tener 8 a 80 caracteres")
-	@NotBlank(message = "Campo titulo no puede estar vacío")
     @Nullable
 	String title,
 
 	@Size(min = 8, max = 140, message = "El contenido debe tener 8 a 140 caracteres")
-	@NotBlank(message = "Campo contenido no puede estar vacío")
     @Nullable
 	String content,
 
@@ -23,7 +19,6 @@ public record UpdatePostDTO (
     @Nullable
     Set<Long> categories,
 
-    @NotNull(message = "Falta el campo del estado")
     @Nullable
     Boolean status
 
