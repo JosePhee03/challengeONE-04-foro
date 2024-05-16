d25da72c4d3d0af16ff21e5b9bb1f1d9899b6eb5
José Luis Gutierrez <joselugz03@gmail>
2024-05-16 17:12:51 -0300

    update to query jpa

--- backend/src/main/java/com/alura/foro/api/infrastructure/adapter/PostJpaRepositoryMySQL.java
+++ backend/src/main/java/com/alura/foro/api/infrastructure/adapter/PostJpaRepositoryMySQL.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.alura.foro.api.infrastructure.entity.PostEntity;
@@ -14,18 +13,18 @@ import com.alura.foro.api.infrastructure.entity.PostEntity;
 public interface PostJpaRepositoryMySQL extends JpaRepository<PostEntity, Long> {
 
     @Query(value = """
-        SELECT p.* FROM post p 
-        LEFT JOIN post_categories pc ON p.id = pc.post_id 
-        LEFT JOIN comment c ON p.id = c.post_id 
+        SELECT p FROM PostEntity p 
+        LEFT JOIN p.categoryEntities pc 
+        LEFT JOIN p.commentEntities c 
         WHERE (:status IS NULL OR p.status = :status) 
-        AND (p.title ILIKE %:query% OR p.content ILIKE %:query%) 
-        AND (:categories IS NULL OR pc.category_id IN (:categories)) 
-        AND (:userId IS NULL OR p.user_id = :userId)
-        """, nativeQuery = true)
+        AND (p.title LIKE %:query% OR p.content LIKE %:query%) 
+        AND (:categories IS NULL OR pc.id IN :categories) 
+        AND (:userId IS NULL OR p.userEntity.id = :userId)
+        """)
     List<PostEntity> searchPosts (
-        @Param("query") String query,
-        @Param("status") Boolean status,
-        @Param("categories")Set<Long> categories,
-        @Param("userId")Long userId);
+        String query,
+        Boolean status,
+        Set<Long> categories,
+        Long userId);
 
 }
